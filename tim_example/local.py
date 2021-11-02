142e7788081a66357718a07b384a734a12ab4889
Tim Zhu <53026628+lodgepolepines@users.noreply.github.com>
2021-11-02 12:03:41 -0400

    Update local.py

--- tim_example/local.py
+++ tim_example/local.py
@@ -3,7 +3,7 @@ from upload import ABUploader
 
 load_dotenv()
 # Change these variables.
-upload_file = 'C:/Users/iamti/Downloads/ab_uploader_test.csv' # path to test CSV
+upload_file = 'ab_uploader_test.csv' # path to test CSV
 config_file = 'config.example.yml'
 campaign_key = 'upload-test'
 config = ABUploader.parse_config(config_file, campaign_key)
